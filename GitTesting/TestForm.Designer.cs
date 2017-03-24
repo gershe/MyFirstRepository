6fd1f4d64aaeea54fb31d665a4e116278f7d72ce
Ed Gersh <gershe@gmail.com>
2017-03-24 07:55:14 -0400

    Fixed up code

--- GitTesting/TestForm.Designer.cs
+++ GitTesting/TestForm.Designer.cs
@@ -29,25 +29,22 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 34);
+            this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // TestForm
-            // 
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 35);
+            this.checkBox1.Location = new System.Drawing.Point(12, 71);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
@@ -59,9 +56,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.checkBox1);
-            this.Name = "TestForm";
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
             this.Name = "TestForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
